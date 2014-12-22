89a94f5906283355b4537f617c68dc637e44ad62
Marin Atanasov <tyxla@abv.bg>
2014-12-22 12:24:59 +0200

    add title to module

--- templates/module-overview.php
+++ templates/module-overview.php
@@ -3,6 +3,7 @@ $data = $wp_uploads_module->get_data();
 $data_labels = $wp_uploads_module->get_data_labels();
 ?>
 <div class="wp-uploads-stats-module wp-uploads-stats-module-overview">
+	<h3>Overview</h3>
 	<table>
 		<tbody>
 			<?php foreach ($data as $key => $value): ?>
