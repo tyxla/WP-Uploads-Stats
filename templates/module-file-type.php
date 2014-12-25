f872ab3949bc80016fb79bf79183cc03018686f1
Marin Atanasov <tyxla@abv.bg>
2014-12-25 11:21:46 +0200

    file type module - add column headings

--- templates/module-file-type.php
+++ templates/module-file-type.php
@@ -4,6 +4,12 @@ $data = $wp_uploads_module->get_data();
 <div class="wp-uploads-stats-module wp-uploads-stats-module-overview">
 	<h3>Files by Type</h3>
 	<table>
+		<thead>
+			<tr>
+				<th class="tl">File type</th>
+				<th class="tr">Number of files</th>
+			</tr>
+		</thead>
 		<tbody>
 			<?php foreach ($data as $key => $value): ?>
 				<tr>
