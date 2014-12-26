2ebf70933d30189a36ea66cbf07e47f5ba9d703a
Marin Atanasov <tyxla@abv.bg>
2014-12-26 18:24:39 +0200

    overview module - total attachments; total size in bytes

--- modules/overview/module.php
+++ modules/overview/module.php
@@ -19,11 +19,18 @@ class WP_Uploads_Stats_Module_Overview extends WP_Uploads_Stats_Module_Base {
 		// get total size
 		$data['total_size'] = size_format($iterator->get_size());
 
+		// get total size (bytes)
+		$data['total_size_bytes'] = $iterator->get_size() . ' B';
+
 		// get number of files
 		$data['total_files'] = $iterator->get_file_number();
 
 		// get number of directories - 1 (main uploads directory excluded)
 		$data['total_directories'] = $iterator->get_directory_number() - 1;
+
+		// get number of attachments
+		$attachment_count = wp_count_posts('attachment');
+		$data['total_attachments'] = $attachment_count->inherit;
 
 		// distribute data to module
 		$this->set_data($data);
@@ -37,8 +44,10 @@ class WP_Uploads_Stats_Module_Overview extends WP_Uploads_Stats_Module_Base {
 	public function get_data_labels() {
 		return array(
 			'total_size' => 'Total Size',
+			'total_size_bytes' => 'Total Size (bytes)',
 			'total_files' => 'Total Number of Files',
 			'total_directories' => 'Total Number of Directories',
+			'total_attachments' => 'Total Attachments',
 		);
 	}
 
