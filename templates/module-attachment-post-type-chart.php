6db2068b98943c25fa1950edf99bb83a98411ab0
Marin Atanasov <tyxla@abv.bg>
2015-01-09 02:32:04 +0200

    template for the Attachment by Post Type - Chart module

---
+++ templates/module-attachment-post-type-chart.php
@@ -0,0 +1,13 @@
+<?php
+$data = $wp_uploads_module->get_data();
+?>
+<div class="wp-uploads-stats-module wp-uploads-stats-module-chart wp-uploads-stats-module-attachments-post-type-chart" id="wpus-module-<?php echo $wp_uploads_module->get_name(); ?>">
+	<?php 
+	$title = 'Attachments by Post Type - Chart';
+	$wp_uploads_module->render_head($title); 
+	?>
+
+	<div class="module-inner <?php echo $wp_uploads_module->is_hidden() ? 'hidden' : ''; ?>">
+		<canvas class="chart chart-pie" id="module-attachments-post-type-chart" data-data='<?php echo json_encode($data); ?>' width="280" height="280" />
+	</div>
+</div>
