25a92f9066eddb65481b97d16bbaebb3405f7b9a
Marin Atanasov <tyxla@abv.bg>
2015-01-10 18:27:54 +0200

    allow menu title/ID to be accessible

--- core/class-module-manager.php
+++ core/class-module-manager.php
@@ -65,16 +65,42 @@ class WP_Uploads_Stats_Module_Manager {
 	}
 
 	/**
+	 * Get the title of the submenu item page.
+	 *
+	 * @access public
+	 *
+	 * @return string $menu_title The title of the submenu item.
+	 */
+	public static function get_menu_title() {
+		// allow filtering the title of the submenu page
+		$menu_title = apply_filters('wp_uploads_stats_menu_item_title', 'Uploads Stats');
+
+		return $menu_title;
+	}
+
+	/**
+	 * Get the ID (slug) of the submenu item page.
+	 *
+	 * @access public
+	 *
+	 * @return string $menu_id The ID (slug) of the submenu item.
+	 */
+	public static function get_menu_id() {
+		$menu_title = self::get_menu_title();
+		return 'wpus-' . sanitize_title_with_dashes($menu_title);
+	}
+
+	/**
 	 * Add the main plugin submenu page
 	 *
 	 * @access public
 	 */
 	public function add_submenu_page() {
-		// the title of the submenu page
-		$menu_title = apply_filters('wp_uploads_stats_menu_item_title', 'Uploads Stats');
+		$menu_title = self::get_menu_title();
+		$menu_id = self::get_menu_id();
 
 		// register the submenu page - child of the Media parent menu item
-		add_submenu_page( 'upload.php', $menu_title, $menu_title, 'manage_options', 'wpus-' . sanitize_title_with_dashes($menu_title), array($this, 'render') );
+		add_submenu_page( 'upload.php', $menu_title, $menu_title, 'manage_options', $menu_id, array($this, 'render') );
 	}
 
 	/**
