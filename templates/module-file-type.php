6c07c19a1c3e49c02957e1752a168080d585fa3f
Marin Atanasov <tyxla@abv.bg>
2015-01-17 23:08:02 +0200

    trim longer file types in files by type module

--- templates/module-file-type.php
+++ templates/module-file-type.php
@@ -1,6 +1,8 @@
 <?php
 $data = $wp_uploads_module->get_data();
 $module_name = $wp_uploads_module->get_name();
+$max_chars = 20;
+$more = '...';
 ?>
 <div class="wp-uploads-stats-module wp-uploads-stats-module-<?php echo $module_name; ?>" id="wpus-module-<?php echo $module_name; ?>">
 	<?php $wp_uploads_module->render_head(); ?>
@@ -14,10 +16,10 @@ $module_name = $wp_uploads_module->get_name();
 				</tr>
 			</thead>
 			<tbody>
-				<?php foreach ($data as $key => $value): ?>
+				<?php foreach ($data as $file_type => $value): ?>
 					<tr>
-						<th>.<?php echo $key; ?></th>
-						<td><?php echo $value; ?></td>
+						<th>.<?php echo strlen($file_type) > $max_chars ? substr($file_type, 0, $max_chars) . $more : $file_type; ?></th>
+						<td><?php echo $value; ?></td> 
 					</tr>
 				<?php endforeach; ?>
 			</tbody>
